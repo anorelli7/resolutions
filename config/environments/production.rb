2727d9fdbae25e7ff06f63b64388f2a39548972f
Andrew Norelli <andrewnorelli@mac.com>
2013-12-22 20:26:13 -0500

    Added devise functionality

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ TestApp::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host => 'http://tranquil-refuge-7345.herokuapp.com/' }
 end
