180156dc6e880ba239b1546bc73b611d955389e5
Andrew Norelli <andrewnorelli@mac.com>
2013-12-30 16:54:49 -0500

    Fixed heroku bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+
          has_many :goals
+
+         validates :name, presence: true
 end
